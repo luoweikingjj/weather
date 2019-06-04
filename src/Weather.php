0d6bf1dc3e5a4f9d71ce3255ef9b282cde966f0d
luowei <luoweikingjj@gmail.com>
2019-06-04 16:20:27 +0800

    从接口中获取天气数据

---
+++ src/Weather.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Luoweikingjj\Weather;
+
+use GuzzleHttp\Client;
+
+/**
+ * Class Weather
+ * @package Luoweikingjj\Weather
+ */
+class Weather
+{
+    /**
+     * @var string
+     */
+    private $key;
+    /**
+     * @var array
+     */
+    private $guzzleOptions = [];
+
+    /**
+     * Weather constructor.
+     * @param string $key
+     */
+    public function __construct(string $key)
+    {
+        $this->key = $key;
+    }
+
+    /**
+     * @return Client
+     */
+    public function getHttpClient()
+    {
+        return new Client($this->guzzleOptions);
+    }
+
+    /**
+     * @param array $options
+     */
+    public function setGuzzleOptions(array $options)
+    {
+        $this->guzzleOptions = $options;
+    }
+
+    /**
+     * @param $city
+     * @param string $type
+     * @param string $format
+     * @return mixed|string
+     */
+    public function getWeather($city, string $type = 'base', string $format = 'json')
+    {
+        $url = 'https://restapi.amap.com/v3/weather/weatherInfo';
+
+        $query = array_filter([
+            'key' => $this->key,
+            'city' => $city,
+            'output' => $format,
+            'extensions' => $type,
+        ]);
+
+        $response = $this->getHttpClient()->get($url, [
+            'query' => $query,
+        ])->getBody()->getContents();
+
+        return 'json' === $format ? \json_decode($response, true) : $response;
+    }
+}
